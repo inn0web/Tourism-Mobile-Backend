08b1b69da305d70700da994711e4b2aa1908c43e
deji12 <adesolaayodeji53@gmail.com>
2025-03-22 14:57:09 +0100

    Created an app to manage places and location based endpoints

--- Tourism/settings.py
+++ Tourism/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'User',
+    'Places',
 ]
 
 MIDDLEWARE = [
--- Tourism/urls.py
+++ Tourism/urls.py
@@ -20,6 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('user/', include('User.urls')),
+   path('places/', include('Places.urls')),
 
    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
