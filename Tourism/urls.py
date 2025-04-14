5d4ef4ce94c7cf94a87ba7edaa264f87abc6c151
deji12 <adesolaayodeji53@gmail.com>
2025-04-14 23:44:14 +0100

    updated swagger base url to https version

--- Tourism/urls.py
+++ Tourism/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from decouple import config
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -15,6 +16,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   url=config("SWAGGER_DOCS_BASE_URL"),
 )
 
 urlpatterns = [
