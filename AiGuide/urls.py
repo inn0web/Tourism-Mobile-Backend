c36eab1eda3490ff274197b224b019b94a10ff95
deji12 <adesolaayodeji53@gmail.com>
2025-07-20 18:13:53 +0100

    socket fix

--- AiGuide/urls.py
+++ AiGuide/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('ai-test/', views.test_ai_guide),
-    path('docs/ai-guide/', views.WebSocketMockAPIView.as_view()),
+    path('docs/ai/', views.WebSocketMockAPIView.as_view()),
     path('get-user-threads/', views.get_user_threads),
     path('get-thread-messages/<str:thread_id>/', views.get_thread_messages),
 ]
